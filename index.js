5addd9496c9051ea069bc4b13297c492b4fa228c
Arturo Alba García <arturo.s593673@cesurformacion.com>
2025-10-01 10:15:28 +0200

    add update()

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('tetris')
 
 const ctx = canvas.getContext('2d')
 
+const starterOffsetY = 2
+
 const boardConfig = {
   height: 20,
   width:10,
@@ -61,48 +63,79 @@ const userConfig = {
   timeBrickDown: 500
 }
 
-const randomShape = Math.floor(Math.random() * shape.length)
+let randomShape;
 
-function draw(shapeSize) {
+function selectRandomShape() {
+  randomShape = Math.floor(Math.random() * shape.length)
+}
+
+function draw() {
+  console.log(randomShape)
   shape[randomShape].forEach((row, y) => {
     row.forEach((value, x) => {
       if(value === 1) {
-        board[ boardConfig.offsetY +y][boardConfig.offsetX + x] = value
+        board[boardConfig.offsetY + y][boardConfig.offsetX + x] = value
       }
+      board[boardConfig.offsetY + y - 1][boardConfig.offsetX + x - 1] = 0
     })
   })
 
   board.forEach((row, y) => {
     row.forEach((value, x) => {
       if(value === 1) {
-        console.log(board, (boardConfig.offsetY + y) * shapeSize, canvas.height)
+        ctx.clearRect(0,0, canvas.width, canvas.height)
         ctx.fillStyle = "yellow"
-        ctx.fillRect((boardConfig.offsetX + x -4) * shapeSize, (boardConfig.offsetY + y) * shapeSize, shapeSize, shapeSize)
+        ctx.fillRect((boardConfig.offsetX) * boardConfig.shapeSize, (boardConfig.offsetY) * boardConfig.shapeSize, boardConfig.shapeSize, boardConfig.shapeSize)
       }
     })
   })
+}
 
-    document.addEventListener('keydown', e => {
+console.log(board)
+
+function solidifyShape(y, x){
+  board[y][x] = 1
+}
+
+function busEvent() {
+  document.addEventListener('keydown', e => {
     if (e.key === 'ArrowLeft') {
-      console.log('ArrowLeft', boardConfig.offsetX)
       boardConfig.offsetX = --boardConfig.offsetX
+      console.log('ArrowLeft', boardConfig.offsetX)
     } else if (e.key === 'ArrowRight') {
       boardConfig.offsetX = ++boardConfig.offsetX
+      console.log('ArrowRight', boardConfig.offsetX)
     } else if (e.key === 'ArrowDown') {
       boardConfig.offsetY = ++boardConfig.offsetY
+      console.log('ArrowDown', boardConfig.offsetY) 
     }
   })
 }
 
+function dropShape() {
+  setInterval(() => {
+    if(boardConfig.offsetY >= board.length - 1) {
+      boardConfig.offsetY = starterOffsetY
+      selectRandomShape()
+      brickState.alive = false
+    } else {
+      brickState.alive = true
+      boardConfig.offsetY++
+    }
+    console.log("setInterval", boardConfig.offsetY)
+  }, 500)
+}
+
 function update() {
+  draw()
   requestAnimationFrame(update)
 }
 
-
-
 function main(){
-  draw(boardConfig.shapeSize)
-  requestAnimationFrame(update)
+  busEvent()
+  selectRandomShape()
+  update()
+  dropShape()
 }
 
 main()
